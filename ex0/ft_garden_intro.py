e2dcdbde770c3b244e5934516d090dae14ea5753
yuttto0024 <yuto0024@outlook.com>
2026-02-16 12:39:09 +0900

    feat/ex0

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,17 @@
+def ft_garden_intro() -> None:
+    """
+    Display the introductory information of the garden.
+    """
+    name = "Rose"
+    height = 25
+    age = 30
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {name}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
+    print()
+    print("=== End of Program  ===")
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
