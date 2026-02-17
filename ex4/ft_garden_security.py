199bceb62356436293374c6c40ad279049a612f7
yuttto0024 <yuto0024@outlook.com>
2026-02-17 19:25:05 +0900

    feat/ex4途中

---
+++ ex4/ft_garden_security.py
@@ -0,0 +1,14 @@
+class Plant:
+    """
+    ~~
+    """
+    def __init__(self, name: str, height: int, age: int)
+        """
+        ~~
+        """
+
+    def set_height(self)
+
+    def set_age(self)
+
+    def
